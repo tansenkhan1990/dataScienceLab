5db07e98c96361a40be0a6c4eed7e0e5a059ace7
tansen <tansenkhan1990@gmail.com>
2020-11-20 20:00:14 +0100

    test commit and push

--- datapreparepdate.py
+++ datapreparepdate.py
@@ -2,12 +2,10 @@ import pandas as pd
 
 import numpy as np
 
-import torch as torch
-
 data = pd.read_csv(
     "/home/tansen/my files/dataScienceLab/GCP_COVID19_MERGED.csv"
 )
-print("**********************************")
+print(" I love VS Code")
 data = data.fillna(0)
 for col in data.columns:
     print(col)
