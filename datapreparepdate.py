1bd0714c7176fbd987d3d3fc0885afc65ea6b980
tansen <tansenkhan1990@gmail.com>
2020-11-20 23:39:29 +0100

    print column name

--- datapreparepdate.py
+++ datapreparepdate.py
@@ -7,7 +7,7 @@ import torch as torch
 data = pd.read_csv(
     "/home/tansen/my files/dataScienceLab/GCP_COVID19_MERGED.csv"
 )
-print(" I love VS Code")
+
 data = data.fillna(0)
 for col in data.columns:
-    print(col)
+    print("columnName: ",col)
