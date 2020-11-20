45427a57ad5810518a382581edd78cd675a26b4f
tansen <tansenkhan1990@gmail.com>
2020-11-20 23:50:39 +0100

    add data len

--- datapreparepdate.py
+++ datapreparepdate.py
@@ -2,12 +2,9 @@ import pandas as pd
 
 import numpy as np
 
-import torch as torch
 
 data = pd.read_csv(
     "/home/tansen/my files/dataScienceLab/GCP_COVID19_MERGED.csv"
 )
 
-data = data.fillna(0)
-for col in data.columns:
-    print("columnName: ",col)
+print(len(data))
