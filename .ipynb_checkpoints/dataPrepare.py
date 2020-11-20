1a627738e9841ed00d41002cf273e479e6fec9d5
tansen <tansenkhan1990@gmail.com>
2020-11-20 19:24:44 +0100

    add numpy

--- .ipynb_checkpoints/dataPrepare.py
+++ .ipynb_checkpoints/dataPrepare.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 
 import torch as torch
 
+import numpy as np
+
 # data = pd.read_csv(
 #     "https://storage.googleapis.com/covid19-open-data/v2/main.csv",
 #     usecols=["urban_area","life_expectancy","infant_mortality_rate","adult_male_mortality_rate",
